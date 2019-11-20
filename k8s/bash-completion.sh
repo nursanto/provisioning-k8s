a7e7a1b94e213b006a1433273a2640c3cbd8a16f
nursanto <nursanto25@gmail.com>
2019-11-20 11:34:08 +0700

    add

--- k8s/bash-completion.sh
+++ k8s/bash-completion.sh
@@ -6,3 +6,7 @@ kubectl completion bash > /etc/bash_completion.d/kubectl
 echo 'alias k=kubectl' >>~/.bashrc
 echo 'complete -F __start_kubectl k' >>~/.bashrc
 
+
+echo 'source <(helm completion bash)' >>~/.bashrc
+helm completion bash > /etc/bash_completion.d/helm
+
