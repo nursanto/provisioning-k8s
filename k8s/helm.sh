927c2a18e1b8108e04e587c6885e41dfafb791c1
nursanto <nursanto25@gmail.com>
2020-03-23 12:39:43 +0700

    Update helm.sh

--- k8s/helm.sh
+++ k8s/helm.sh
@@ -1,6 +1,7 @@
 wget https://get.helm.sh/helm-v2.16.0-linux-amd64.tar.gz
 tar xzf helm-v2.16.0-linux-amd64.tar.gz
 cd linux-amd64/
+cp helm /usr/local/bin/
 kubectl -n kube-system create serviceaccount tiller
 kubectl create clusterrolebinding tiller --clusterrole cluster-admin --serviceaccount=kube-system:tiller
 helm init --service-account tiller
